e648d2d88305b2ab5970b7733697e2853aefc840
FrankieMarie <frankiemarie83@gmail.com>
2019-03-02 12:52:04 -0700

    responsive header

--- client/src/App.js
+++ client/src/App.js
@@ -1,7 +1,5 @@
 import React, { Component } from 'react';
 import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
-import { Provider } from 'react-redux';
-import store from './redux/store';
 
 import { imagePaths } from './assets/imagePaths/index';
 
@@ -11,22 +9,20 @@ import Projects from './components/Projects';
 class App extends Component {
   render() {
     return (
-      <Provider store={store}>
-        <Router>
-          <div className="App">
-            <Switch>
-              <Route exact path="/" component={Main} />
-              <Route
-                exact
-                path="/projects"
-                render={ () =>
-                  <Projects images={imagePaths} />
-                }
-              />
-            </Switch>
-          </div>
-        </Router>
-      </Provider>
+      <Router>
+        <div className="App">
+          <Switch>
+            <Route exact path="/" component={Main} />
+            <Route
+              exact
+              path="/projects"
+              render={ () =>
+                <Projects images={imagePaths} />
+              }
+            />
+          </Switch>
+        </div>
+      </Router>
     );
   }
 }
